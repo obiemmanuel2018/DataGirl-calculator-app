6174ad23d508a347e42d176258e5dde6698f6783
obiemmanul2018 <obiemmanuel2018@gmail.com>
2022-08-03 16:11:17 +0100

    added all the button operations on our calculator app

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,65 @@
-let currentResult = 0;
 
-const userFirstInput = 100;
-userFirstInput = 120;
-let operator = '+';
-let userSecondInput = 220;
 
-currentResult = userFirstInput + userSecondInput;
-let result_description = `${userFirstInput} ${operator} ${userSecondInput}`
-outputResult(currentResult, result_description);
+// GLOBAL SCOPE
+let currentResult = 0
+let logResult = []
+
+
+// Get's user input.
+function getUserInput(){
+   return parseInt(userInput.value)
+}
+
+// Displays calculation description
+function getDescription(operator){
+   return `${currentResult} ${operator} ${getUserInput()}`
+}
+
+function add(){ 
+  const calcDescription = getDescription('+');
+  currentResult +=  getUserInput();
+  let resultObj = {
+    result: currentResult,
+    description: calcDescription
+  }
+  logResult.push(resultObj)
+  outputResult(currentResult, calcDescription)
+}
+
+
+addBtn.addEventListener('click', add)
+
+function subtract(){
+    const calcDescription = getDescription('-')
+    currentResult -= getUserInput();
+    outputResult(currentResult, calcDescription)
+}
+
+subtractBtn.addEventListener('click',subtract)
+
+
+function multiply(){
+    const calcDescription = getDescription('*')
+    currentResult *= getUserInput();
+    outputResult(currentResult, calcDescription)
+}
+
+multiplyBtn.addEventListener('click', multiply)
+
+function divide(){
+    const calcDescription = getDescription('/')
+    currentResult /= getUserInput();
+    outputResult(currentResult, calcDescription)
+}
+
+divideBtn.addEventListener('click', divide);
+
+
+// Numbers
+// Strings
+// Boolean 
+// Arrays 
+// Objects
+
+
+
