8c344d6edecfac12458fd1132f5ac39dd5aeb5c9
obiemmanul2018 <obiemmanuel2018@gmail.com>
2022-08-03 12:37:15 +0100

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+let currentResult = 0;
+
+const userFirstInput = 100;
+userFirstInput = 120;
+let operator = '+';
+let userSecondInput = 220;
+
+currentResult = userFirstInput + userSecondInput;
+let result_description = `${userFirstInput} ${operator} ${userSecondInput}`
+outputResult(currentResult, result_description);
